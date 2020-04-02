2ae590b5b8f7915546040a8abc8c61c1069490ae
atefeh <ati_atashi1993@yaoo.com>
2020-04-02 13:55:16 +0430

    Insert GeneratePassword component to app view

---
+++ generate-password/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <GeneratePassword/>
+  </div>
+</template>
+
+<script>
+
+import GeneratePassword from './components/GeneratePassword';
+
+export default {
+  name: 'App',
+  components: {
+    GeneratePassword,
+  }
+}
+</script>
+<style>
+  @import './assets/style.css';
+  @import '~material-design-icons/iconfont/material-icons.css';
+</style>
